0ca90b53fab2b543d95c63b5114f7ad5e1c35a1d
Valdas <valdas.kestavicius@gmail.com>
2020-06-09 21:56:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      int integeris = scanner.nextInt();
+
+      System.out.println(integeris);
+    }
+}
